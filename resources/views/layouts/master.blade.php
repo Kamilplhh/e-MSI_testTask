27ae2d2cb5e8b15578ebf24d3fa7daa8158a3aed
Kamil <kamilczekotas@gmail.com>
2023-09-04 16:53:28 +0200

    feat: left panel done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,26 @@
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <title>Zadanie zdalne e-MSI</title>
 
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
 </head>
 
 <body>
-    <div class="left">
-        
+    <div id="Lewy">
+        <a href="/">Różne kontrolki HTML</a>
+        <a href="/pracownicy">Tabela Pracowników</a>
+        <a href="/faktury">Tabela Faktur VAT</a>
+        <a href="/delegacje">Tabela Delegacji BD</a>
+        <a href="/kontrahenci">Dane Kontrahentów</a>
     </div>
 
-    <div class="right">
+    <div id="Prawy">
         @yield('content')
     </div>
 </body>
